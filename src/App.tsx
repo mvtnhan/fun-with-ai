f2d47edfae2ea130b8e226bd81da217aaa0095e1
mvtnhan <mvtnhan@gmail.com>
2022-05-13 12:53:50 +0700

    add Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="App">
       <h1>hi mvtnhan</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
